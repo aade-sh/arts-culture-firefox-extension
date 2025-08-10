7fc20050c9046ff6ce3daea985e623f6c2e40215
aade-sh <aadeshchandra01@gmail.com>
2025-08-10 17:11:12 +0530

    update info icon

--- src/components/ArtDisplay.tsx
+++ src/components/ArtDisplay.tsx
@@ -111,9 +111,11 @@ export function ArtDisplay({
               strokeLinecap="round"
               strokeLinejoin="round"
             >
-              <circle cx="12" cy="12" r="10" />
-              <path d="M12 16v-4" />
-              <path d="M12 8h.01" />
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
+              />
             </svg>
           </button>
           <button
